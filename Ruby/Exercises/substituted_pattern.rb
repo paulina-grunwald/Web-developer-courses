39d8d760a4c251891d23f0a9a82440617a9f2e8f
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-01-05 20:29:03 +0000

    Add comments

--- Ruby/Exercises/substituted_pattern.rb
+++ Ruby/Exercises/substituted_pattern.rb
@@ -8,11 +8,11 @@ Used methods:
 =end
 
 print "Please insert a sentence. A will be replaced with X:"
-user_input = gets.chomp
-user_input.downcase!
+user_input = gets.chomp # take user input
+user_input.downcase! # change the string to lowercase
 
-if user_input.include? "a"
-  user_input.gsub!(/a/, "x")
+if user_input.include? "a" # test if string include letter "a"
+  user_input.gsub!(/a/, "x") # substitute letter "a" to "x"
 else
   puts user_input
 end
