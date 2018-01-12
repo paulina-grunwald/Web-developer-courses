7aceaf1d3e00ae8ef6c554b03ec8ff95d3835184
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-01-12 21:16:32 +0000

    update exercise on hashes
    add example of methods:
    .fetch,
    .to_sym,
    .to_s,

--- Ruby/Exercises/hashes.rb
+++ Ruby/Exercises/hashes.rb
@@ -30,3 +30,36 @@ p cities["Netherlands"]
 #check what happens when you ask about non existent objects
 p cities["Turkey"]
 #=>nil
+
+puts
+
+
+
+##### Intro to Symbols and Symbols as Hash Keys #####
+#Symbols are often used as keys for hashes as they are more lightweight.
+
+p :name.class
+#=>symbol
+person = {name: "Paulina",
+          age:20,
+          languages: ["Python", "Ruby", "PHP"]}
+
+
+#convert symbol to string
+p :age.to_s.class
+#=>string
+p "age".to_sym.class
+#=>symbol
+
+#####  The .fetch Method on a Hash #####
+
+candy = {icecream: 1.5,
+         chips: 2,
+         chocolate: 3}
+
+p candy.fetch(:icecream)
+#=> 1.5
+p candy.fetch(:chocolate)
+#=>3
+p candy.fetch(:lolipop, 9.99 )
+#=>9.99
