668fba1f7d9044651ae5e9bb2585d46b5865891b
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-01-19 13:24:48 +0000

    update script.js
    create constructor function, add cat class

--- JavaScript Objects and Prototypes (Pluralsight)/script.js
+++ JavaScript Objects and Prototypes (Pluralsight)/script.js
@@ -9,3 +9,49 @@ var cat = {
 cat.age = 3
 
 cat.speak()
+
+
+// create constructor function
+function Cat(name, color) {
+	this.name = name
+	this.color = color
+
+}
+
+var cat = new Cat('Fluffy', 'White');
+display(cat)
+
+
+
+
+
+
+var cat = Object.create(Object.prototype,
+	{
+		name: { 
+			value: 'Fluffy',
+			enumerable: true,
+			writable: true,
+			configurable: true
+		},
+		color: {
+			value: 'White',
+			enumerable: true,
+			writable: true,
+			configurable: true
+		}
+	})
+
+
+class Cat {
+	constructor(name, color) {
+		this.name = name
+		this.color = color
+	}
+
+	speak() {
+		display('Meeeeeeow')
+	}
+}
+
+cat.speak()
