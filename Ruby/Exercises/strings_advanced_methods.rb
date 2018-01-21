031801fc3bd867339885ee301907b6899adba60c
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-01-21 18:03:48 +0000

    Update strings_advanced_methods.rb
    Add more code examples that use following methods:
    - .index,
    - .insert,
    - .squeeze
    - .clear
    - .delete

--- Ruby/Exercises/strings_advanced_methods.rb
+++ Ruby/Exercises/strings_advanced_methods.rb
@@ -34,7 +34,13 @@ letter.each {|letter| puts "#{letter} is cool!"}
 #=>a is cool!
 #=> x is cool!
 
+
+
+
 puts
+
+
+
 
 ##### The .join Method on an Array #####
 
@@ -102,12 +108,140 @@ p custom_count(sentence, "a")
 
 
 
+puts
+
+
+
+
 ##### The .index and .rindex Methods on a String####
 
+sentence = "I love Ruby a lot"
+
+# Find an index position of letter "I"
+p sentence.index("I")
+#<= 0
+p sentence.index("o")
+#<= 3 // returns first position of "o"
+
+# Find the index position of 2nd "o" letter in the sentence string
+p sentence.index("o", 4)
+#<= 15
+
+# .rindex works the same way as index but
+# it works from the end of the string to the beginning.
+# So actually it's revrese index method.
+
+p sentence.rindex("o")
+#<= 15
+
+puts
+
+
+def custom_index(string, substring)
+  return nil unless string.include?(substring)
+    length = substring.length
+    string.chars.each_with_index do |char, index|
+      sequence = string[index, length]
+      return index if sequence == substring
+  end
+end
+
+p custom_index(sentence, "o")
+#<= 3
+
+
+
+
+puts
+
+
+
 ##### The .insert Method on a String #####
+# .insert method inserts a character or a string at specified index place in a another strings
+# .inserts mutates original strings
+typo = "GeorgWashington"
+typo.insert(5, "e")
+p typo
+
+
+
+puts
+
+
 
 ##### The .squeeze Method #####
+# .squeeze method removes all characters that occur more than once
+# in a string
+
+sentence = "II loove Ruuuby"
+puts sentence.squeeze
+#<= I love Ruby
+
+sentence_1 = "I loooove Ruby"
+
+puts sentence_1.squeeze("o")
+#<= I love Ruby
+
+
+def custom_squeeze(string)
+  return string.squeeze
+
+end
+
+p custom_squeeze(sentence)
+#<= "I love Ruby"
+
+
+words = "Ruuby is tthee proograming laaanguage"
+def squeeze(string)
+  # Create empty string
+  final = ""
+  # Split by characters
+  chars = string.split("")
+  # Iterate over each character taking into the consideration index
+  chars.each_with_index {|char, index| char == chars[index+1] ? next : final << char}
+  final
+end
+
+puts squeeze(words)
+#<= Ruby is the programing language
+
+
+
+puts
 
 #####  The .clear Method on a String #####
+# .clear method clears the content of the string and returns empty strings
+# .clear mutates original string
+
+word = "ruby"
+word.clear
+
+p word
+#<= ""
+
+
+
+puts
+
+
 
 ##### The .delete Method on a String #####
+# .delete method delete specified characters from the string
+puts "Hello, it's a nice day today".delete("osa")
+#<= Hell, it'  nice dy tdy
+
+
+def custom_delete(string, substring)
+  new_string = ""
+  # Separate substring into chars and put them into array
+  chars = substring.chars
+  # Check if chars are present in the string - if yes delete
+  string.each_char do |char|
+    new_string << char unless substring.include?(char)
+  end
+  new_string
+end
+
+p custom_delete("Hello my name is Mike", "e")
+#<= "Hllo my nam is Mik"
