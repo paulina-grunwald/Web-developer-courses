3c8ccb129e07c96397a65c5e8462ed0cdeb7f662
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-01-24 17:03:05 +0000

    Update hashes.rb

--- Ruby/Exercises/hashes.rb
+++ Ruby/Exercises/hashes.rb
@@ -31,6 +31,9 @@ p cities["Netherlands"]
 p cities["Turkey"]
 #=>nil
 
+
+
+
 puts
 
 
@@ -48,8 +51,18 @@ person = {name: "Paulina",
 #convert symbol to string
 p :age.to_s.class
 #=>string
+
+#convert sting to symbol
 p "age".to_sym.class
 #=>symbol
+
+
+
+
+puts
+
+
+
 
 #####  The .fetch Method on a Hash #####
 
@@ -57,15 +70,108 @@ candy = {icecream: 1.5,
          chips: 2,
          chocolate: 3}
 
+p candy[:chips]
+#<= 2
+
+# Use fetch method on a hash
 p candy.fetch(:icecream)
 #=> 1.5
 p candy.fetch(:chocolate)
 #=>3
-p candy.fetch(:lolipop, 9.99 )
+
+# .fetch is more flexible than just calling the key.
+# If we we input only key name and it's not included in the hashes
+# We will get error. If we provide 2nd arguments
+# It will return 2nd
+
+p candy.fetch(:lolipop, 2) # == candy[:lolipop, nil]
 #=>9.99
 
 
+puts
 
+
+
+
+##### Add a Key Value Pair to a Hash with Bracket Syntax or the .store Method  ######
+
+menu = {burger: 4, taco: 2, salad: 3.44, chips: 1}
+
+p menu[:burger]
+#<= 3
+p menu[:sandwich]
+#<= nil
+
+# Add new value to the hash
+menu[:tortilla] = 5
+p menu
+#<= {:burger=>4, :taco=>2, :salad=>3.44, :chips=>1, :tortilla=>5}
+
+# Use .store methd on the hashe
+menu.store(:sushi, 12.55)
+p menu
+#<= {:burger=>4, :taco=>2, :salad=>3.44, :chips=>1, :tortilla=>5, :sushi=>12.55}
+
+
+# Create priority&to do list in form of hash
+
+todo = {clean_dishes: 1, cook_dinner: 2, wash_car: 3  }
+p todo
+#<= {:clean_dishes=>1, :cook_dinner=>2, :wash_car=>3}
+
+# Add item to the todo listed
+todo.store(:wash_clothes, 4)
+todo[:send_letter] = 3
+
+p todo
+#<= {:clean_dishes=>1, :cook_dinner=>2, :wash_car=>3, :wash_clothes=>4, :send_lette
+
+
+puts
+
+
+
+
+#####  The .length and .empty? Methods on a Hash  #####
+
+shopping_list = {carrot: 2, pasta: 1, apples: 7}
+shopping_list2 = {}
+
+p shopping_list.length
+#<= 3
+
+p shopping_list.empty?
+#<= false
+
+p shopping_list2.length
+#<= 0
+p shopping_list2.empty?
+#<= true
+
+
+
+
+
+puts
+
+
+#####  The .each Method on a Hash #####
+
+
+
+
+
+
+
+
+
+
+puts 
+
+
+
+
+#####  The .each_key and .each_value Methods ######
 
 # Write a function that sorts the keys in a hash by the length
 #of the key as a string. For instance, the hash:
