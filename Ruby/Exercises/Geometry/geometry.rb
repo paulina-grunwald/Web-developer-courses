71fc4db05bc25ca1399761122c799d0d2f7953f5
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-01-26 13:51:16 +0100

    Add geometry.rb
    This file includes modules rectangle.rb, square.rb and circle.rb and can
    call on those modules and perform calculation of the area of selected
    object.

---
+++ Ruby/Exercises/Geometry/geometry.rb
@@ -0,0 +1,7 @@
+# Include modules form rectable.rb, square.rb and circle.rb which are located in the sam efolder
+require_relative "./rectangle.rb"
+require_relative "./square.rb"
+require_relative "./circle.rb"
+
+puts Circle.area(10)
+#<= 3.14
