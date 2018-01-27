8e195d957fea7509b95a3e765797a0acffa08f3d
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-01-27 18:28:44 +0100

    Create script.js for Blackjack game
    This script will be running engine for the Blackjack game. So far i have
    created two arrays for card type and card value. Moreover, DOM variables
    like textArea and various buttons were added to the script. They will
    control start and end of the game and also receiving new cards.

---
+++ Javascript/JavaScript Getting Started/script.js
@@ -0,0 +1,20 @@
+// Blackjack game code
+
+
+// Card variables
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+
+let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten',
+              'Nine', 'Eight', 'Seven', 'Six', Five,
+              'Four', 'Three', 'Two'];
+
+
+// DOM variables
+let textArea = document.getElementById('text-area'),
+    newGameButton = document.getElementById('new-game-button'),
+    hitButton = document.getElementById('hit-button'),
+    stayButton = document.getElementById('stay-button');
+
+
+
+
