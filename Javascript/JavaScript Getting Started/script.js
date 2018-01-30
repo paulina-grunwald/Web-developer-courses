3abbec648e69994bfecbfe2fc810f0092c187cbc
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-01-30 17:02:02 +0100

    Update script.js
    add game variables
    add shuffleDeck funtion - this function will shuffle cards in the deck.
    add show status function

--- Javascript/JavaScript Getting Started/script.js
+++ Javascript/JavaScript Getting Started/script.js
@@ -16,20 +16,49 @@ let textArea = document.getElementById('text-area'),
     hitButton = document.getElementById('hit-button'),
     stayButton = document.getElementById('stay-button');
 
+
+// Game variables (at the start of the game)
+let gameStarted = false,
+    gameOver = false, 
+    playerWon = false,
+    dealerCards = [],
+    playerCards = [],
+    dealerScore = 0,
+    playerScore = 0,
+    desck = [];
+    
+
 // Hide hitButton and stayButton (this button needs to be hidden before the game is started)
 hitButton.style.display = 'none';
 stayButton.style.display = 'none';
 
-//Set up handler for starting new game
+//Show current status
+showStatus();
 
+// Set up handler for starting new game.   
+// When user clicks on new game the game will be started
 newGameButton.addEventListener('click', function(){
+  //Update game variables
+  gameStarted = true;
+  gameOver = false;
+  playerWon = false;
+
+  // Create new deck of cards
+  deck = createDeck();
+  shuffleDeck(deck);
+
+  // Give two cards for dealer and player
+  dealerCards = [ getNextCard(), getNextCard() ];
+  playerCards = [ getNextCard(), getNextCard() ];
+
   textArea.innerText = 'Started...';
   newGameButton.style.display = 'none'
   hitButton.style.display = 'inline';
   stayButton.style.display = 'inline';
+  showStatus();
 });
 
-//Create deck function
+// Create deck function
 function createDeck() {
   let deck = [];
   // Loop through the suits
@@ -47,17 +76,48 @@ function createDeck() {
   return deck;
 }
 
+function shuffleDeck(deck){
+  for (let i = 0; i < deck.length; i++){
+    // Calc index of the card we want to swap
+    let swapIdx = Math.strunc(Math.random() * deck.length);
+    //Set temporary to temp
+    let tmp = deck[swapIdx]
+    deck[swapIdx] = deck[i]
+    deck[i] = temp;
+  }
+}
+
+// Print out name of given card
+function getCardString(card) {
+  return card.value + ' of ' + card.suit;
+}
+
+
+// Show status
+function showStatus() {
+  // If game is not started then assign new string to textArea
+  if (!gameStarted) {
+    textArea.innerText = 'Welcome to Blackjack!'
+    return
+  }
+  // Give cards to the dealer
+  let dealerCardString = '';
+
+
+  // Give cards to the player
+  let playerCardString =  '';
+
+  textArea.innerText = 
+
+  updateScores();
+
+}
 
 // Get next card from the deck
 function getNextCard() {
   return deck.shift();
 }
 
-
-// Print out name of given card
-function getCardString(card) {
-  return card.value + ' of ' + card.suit;
-}
 
 
 // Create deck
