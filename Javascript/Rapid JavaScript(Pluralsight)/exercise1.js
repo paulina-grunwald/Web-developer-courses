a271d212bc2c1a691e31366502e424c64babb929
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-02-02 12:05:21 +0100

    Add various exercises in javascript

---
+++ Javascript/Rapid JavaScript(Pluralsight)/exercise1.js
@@ -0,0 +1,18 @@
+var orderId = 9001.01;
+console.log(typeof orderId);
+//number
+
+var orderId = "hello";
+console.log(typeof orderId);
+// string
+
+var isActive = true;
+console.log(typeof isActive);
+//boolean
+var isOk;
+console.log(typeof isOk);
+// undefined
+
+var fruits = ['banana', 'apple', 'pear'];
+console.log(typeof fruits);
+//object
