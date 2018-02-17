d6179efdcd071d02df78b1aee7dc331a1bf3255d
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-02-17 14:25:04 +0100

    remove complete and save from the constructor function
    remove complete and save from the constructor function and attach it to
    the Task prototype. That will improve efficency of the code - everytime
    new task is created new complete and save function will not be created.
    All of tasks have access to task.prototype.

--- Javascript/Practical Design Patterns in JavaScript/task_cf.js
+++ Javascript/Practical Design Patterns in JavaScript/task_cf.js
@@ -4,16 +4,17 @@
 var Task = function(name) {
   this.name = name;
   this.completed = false;
-
-  this.complete = function(){
-    console.log('completing task: ' + this.name)
-    this.completed = true;
-  }
-
-  this.save = function() {
-    console.log('saving Task: ' + this.name);
-  }
 }
+
+Task.prototype.complete = function(){
+  console.log('completing task: ' + this.name)
+  this.completed = true;
+};
+
+Task.prototype.save = function() {
+  console.log('saving Task: ' + this.name);
+};
+
 
 var task1 = new Task('Wash dishes');
 var task2 = new Task('Clean floor');
@@ -22,3 +23,5 @@ var task4 = new Task('Cook dinner');
 
 task1.complete();
 task2.save();
+task3.save();
+task4.save();
