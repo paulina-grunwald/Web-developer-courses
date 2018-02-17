1629ab4740d8dddef668950e139220479cdf5fb1
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-02-17 14:28:15 +0100

    remove part of the code and move it to main.js
    remove newly created ask and called complete and save functions and
    place them in main.js file which is located in the same folder as
    task_cf.js

--- Javascript/Practical Design Patterns in JavaScript/task_cf.js
+++ Javascript/Practical Design Patterns in JavaScript/task_cf.js
@@ -14,14 +14,3 @@ Task.prototype.complete = function(){
 Task.prototype.save = function() {
   console.log('saving Task: ' + this.name);
 };
-
-
-var task1 = new Task('Wash dishes');
-var task2 = new Task('Clean floor');
-var task3 = new Task('Do the homework');
-var task4 = new Task('Cook dinner');
-
-task1.complete();
-task2.save();
-task3.save();
-task4.save();
