c2241d68e295d7ebfa111472227dbe86022c45bd
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-03-18 12:28:59 +0100

    add exercise files
    Add exericse file in which i will try to experiment with all things that
    are explained in the course

---
+++ jQuery/jQueryIntro/Exercises/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function (){
+  // find all the span tags
+  var spans = $('span');
+  alert(spans.length);
+
+  // change color of heading1
+  //$('h2, heading1').css('background-color', 'Red');
+  //change color of
+  $('h4').css('background-color', 'Green');
+
+});
