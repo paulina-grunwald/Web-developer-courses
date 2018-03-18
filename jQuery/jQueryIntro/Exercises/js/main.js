fb396b06b8930859ffb766684ce2b7fb810ab868
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-03-18 15:50:35 +0100

    update main.js

--- jQuery/jQueryIntro/Exercises/js/main.js
+++ jQuery/jQueryIntro/Exercises/js/main.js
@@ -3,9 +3,28 @@ $(document).ready(function (){
   var spans = $('span');
   alert(spans.length);
 
-  // change color of heading1
-  //$('h2, heading1').css('background-color', 'Red');
-  //change color of
+  //change color of h4
   $('h4').css('background-color', 'Green');
+
+  //print content of each div
+  //$('div').each(function (){
+  //  alert($(this).html());
+  //});
+
+  //print number of all divs and spans
+  var coll = $('div, span');
+  alert(coll.length);
+
+  // Select element by id
+  alert($("#heading").html());
+  // change color of id=Heading
+  $('#heading').css('background-color', 'Yellow');
+
+  $('.text1').css('border', '2px solid blue');
+
+
+  //select inputs
+  var inputs = $(':input');
+  alert($(inputs[1]).val());
 
 });
