f8b877632bebaffc425b7ea49f05be1446348f4d
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-04-06 18:45:19 +0200

    add various basic exercises using js
    add various basic exercises using javascript

---
+++ Javascript/Various Exercises/beginner-exercises.js
@@ -0,0 +1,31 @@
+// variable
+
+myObject = {
+  firstValue: 'a',
+  secondValue: 'b'
+};
+
+console.log(myObject.firstValue);
+
+
+///////////// Boolean ////////////////
+
+console.log(Boolean(""));
+// false
+console.log(Boolean(0));
+// false
+console.log(Boolean(null));
+// false
+console.log(Boolean(undefined));
+// false
+
+
+console.log(Boolean(-1));
+// true
+console.log(Boolean("a"));
+// true
+console.log(Boolean(" "));
+// true
+
+
+///////////// Functions ////////////////
