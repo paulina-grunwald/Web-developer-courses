9f5ba4bc24e00574823c2aef0dfccf6f05fd96e8
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-04-07 16:30:54 +0200

    exercises using build-in objects
    In this file i will write various exercises using build-in methods on
    objects e.g maths, toLowerCase etc.

---
+++ Javascript/Various Exercises/build-in-objects.js
@@ -0,0 +1,35 @@
+
+///////////////////////// Numbers////////////////////////
+
+/* ------- STRING METHODS  -------*/
+
+let word = "I love to code";
+upcase = word.toUpperCase();
+console.log(upcase);
+// I LOVE TO CODE
+lowcase = word.toLowerCase();
+console.log(lowcase);
+//i love to code
+
+
+/* -------Math.random() anf Math.trunc() -------*/
+
+let number = Math.random() * 10;
+console.log(number);
+// 5.825591019178839
+round = Math.trunc(number);
+console.log(round);
+
+
+
+/* -------Number.isNan() -------*/
+let num = 3;
+let num2 = NaN;
+console.log(Number.isNaN(num));
+// false
+console.log(Number.isNaN(num2));
+// true
+
+
+
+/* ------- DATES  -------*/
