59e699213612e18b1ee60918feb848bb63ac3fe0
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-04-07 09:19:15 +0200

    update file
    add exercises for functions and arrays

--- Javascript/Various Exercises/beginner-exercises.js
+++ Javascript/Various Exercises/beginner-exercises.js
@@ -7,6 +7,50 @@ myObject = {
 
 console.log(myObject.firstValue);
 
+///////////// Escaping characters  ////////////////
+console.log("Paulina'\s code")
+console.log("              ")
+
+
+///////////// typeof  ////////////////
+console.log(typeof("mike"));
+console.log(typeof(2));
+console.log(typeof(null));
+//object
+console.log(typeof(undefined));
+//undefined
+console.log(typeof(true));
+//boolean
+console.log("              ")
+
+///////////// math ////////////////
+console.log(-10/0);
+//-Infinity
+console.log(0/0);
+// NaN
+console.log("              ")
+
+///////////// Arrays  ////////////////
+
+let names = ["Mike", "John", "Lynda"];
+console.log(names[1]);
+console.log(names.length);
+names.push("Paul");
+console.log(names);
+//[ 'Mike', 'John', 'Lynda', 'Paul' ]
+
+names.pop();
+console.log(names);
+// [ 'Mike', 'John', 'Lynda' ]
+
+names.shift();
+console.log(names);
+// [ 'John', 'Lynda' ]
+
+console.log("              ")
+
+
+
 
 ///////////// Boolean ////////////////
 
@@ -29,3 +73,10 @@ console.log(Boolean(" "));
 
 
 ///////////// Functions ////////////////
+
+// Basic funtion structure in javascript
+ var square =  function(x) {
+   return x * x;
+ };
+console.log(square(7));
+// 49
