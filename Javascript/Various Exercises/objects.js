09d15274e76abdc489e0b2e60eb760db134aad73
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-04-08 11:58:59 +0200

    update objects.js
    add exercise on objects:
    - calling item from the object
    - iteration over items in the object

--- Javascript/Various Exercises/objects.js
+++ Javascript/Various Exercises/objects.js
@@ -4,3 +4,32 @@ var animal = {};
 animal.specie = "dog";
 console.log(animal);
 // { specie: 'dog' }
+
+
+// Add items to the object
+
+var box = {};
+box["material"] = "metal";
+
+box["size"] = {
+  "height": 3,
+  "width": 6
+};
+
+box["area"] = box.size.height * box.size.width;
+
+console.log(box);
+// { material: 'metal', size: { height: 3, width: 6 }, area: 18 }
+
+
+// Iteration
+var animal = {};
+animal["type"] = "dog";
+animal["sound"] = "bark";
+animal["age"] = 3;
+
+for(var item in animal) {
+  console.log(item);
+  console.log(animal[item]);
+};
+// type sound age
