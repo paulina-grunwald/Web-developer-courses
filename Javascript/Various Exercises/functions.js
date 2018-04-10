dc359fed9e7ffc8215c8d8aa02048d46aa93e247
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-04-10 20:30:47 +0200

    update functions.js
    add closure example

--- Javascript/Various Exercises/functions.js
+++ Javascript/Various Exercises/functions.js
@@ -20,3 +20,37 @@ for(var i = 0; i < fruitNames.length; i++) {
 for(var i = 0; i < fruitNames.length; i++) {
   fruitsCollection[i].desc();
 }
+
+
+console.log("     ");
+
+
+//////SCOPE
+var closureLog = function() {
+  var x = "I am in the closure";
+  var log = function() {
+    console.log(x);
+
+  };
+  setTimeout(log, 1000);
+  console.log('will run after closure')
+};
+
+closureLog();
+
+
+console.log("    ");
+///
+
+var closureFunc = function() {
+  var x = 0;
+  var alerter = function() {
+    console.log(++x);
+  };
+  return alerter;
+}
+
+var funcStorage = closureFunc();
+funcStorage();
+funcStorage();
+funcStorage();
