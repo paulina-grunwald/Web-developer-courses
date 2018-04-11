b51b239609c4334ebab5fdb0efc8539014603a79
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-04-11 17:40:45 +0200

    update funcions.js

--- Javascript/Various Exercises/functions.js
+++ Javascript/Various Exercises/functions.js
@@ -52,5 +52,42 @@ var closureFunc = function() {
 
 var funcStorage = closureFunc();
 funcStorage();
+// 1
 funcStorage();
+// 2
 funcStorage();
+// 3
+
+
+console.log("    ");
+
+///// Closure example
+
+var add = function(num) {
+  var num1 = num;
+  var addNums = function(num2) {
+      return num1+num2;
+  };
+  return addNums;
+};
+
+
+var add4= add(4);
+console.log(add4(2));
+// 6
+
+
+/// Closure Exercise 2
+console.log("     ");
+
+function counter() {
+  var n = 0;
+  return {
+    count: function() { return n++; },
+    reset: function() { return n = 0; }
+  };
+};
+
+var myCounter = counter();
+myCounter.count();
+//myCounter.reset();
