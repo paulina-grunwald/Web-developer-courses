2a451e788d31d5ac75e96bf257817538e91dd457
paulina_grunwald <paulina.grunwald@gmail.com>
2018-04-20 16:59:33 +0200

    add 3 new components for button, answer and numbers. Use boostrap to style the components.

--- React/num-game/src/App.js
+++ React/num-game/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 import ReactDOM from 'react-dom';
 import 'font-awesome/css/font-awesome.min.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
  
 
 var FontAwesome = require('react-fontawesome');
@@ -13,7 +12,7 @@ var FontAwesome = require('react-fontawesome');
 
 const Stars = (props) => {
   return (
-      <div>
+      <div className="col-5">
         <i className="fa fa-star"></i>
         <i className="fa fa-star"></i>
         <i className="fa fa-star"></i>
@@ -22,14 +21,51 @@ const Stars = (props) => {
     );
 }
 
+const Button = (props) => {
+  return (
+      <div className="col-2">
+        <button>+1</button>
+      </div>
+    );
+}
+
+const Answer = (props) => {
+  return (
+      <div className="col-5">
+        ...
+      </div>
+    );
+}
+
+
+const Numbers = (props) => {
+  return (
+    <div className="card text-center">
+      <div>
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
+        <span>4</span>
+      </div>
+
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h3>Play Nine</h3>
-        <Stars />
-      
+      <div className="App container">
+        <h2>Play Nine</h2>
+        <hr />
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer />
+        </div>
+        <br />
+        <Numbers />
+        
       </div>
     );
   }
