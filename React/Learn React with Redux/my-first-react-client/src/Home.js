7ad7f11abb6b20a4d3133930381de22204c95e13
paulina_grunwald <paulina.grunwald@gmail.com>
2018-04-27 13:44:19 +0200

    add Layout component

--- React/Learn React with Redux/my-first-react-client/src/Home.js
+++ React/Learn React with Redux/my-first-react-client/src/Home.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {App, AppHeader, Title, AppIntro, AppLogo} from './styles';
-import logo from './logo.svg';
-const paragraphStyles = {backgroundColor: "orange"};
+import Layout from './Layout'
+import { AppIntro} from './styles';
 
 export default function Home() {
 	return(
-
-      <App>
-        <AppHeader>
-          <AppLogo src={logo} alt="logo" />
-          <Title>Welcome to React</Title>
-        </AppHeader>
-        <AppIntro>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </AppIntro>
-        <h2>Hot reloading is amazing!</h2>
-      </App>
+      <Layout
+        title="Welcome to React"
+        renderContent={() => (
+          <React.Fragment>
+            <AppIntro>
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </AppIntro>
+            <h2>Hot reloading is amazing!</h2>
+          </React.Fragment>
+        )} 
+      />
     );
-	
 }
