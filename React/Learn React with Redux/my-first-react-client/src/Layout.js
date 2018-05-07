842b5a3064e35b3f3421a239cc35bfd6116c910e
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-05-07 18:59:47 +0200

    add Layout.js

---
+++ React/Learn React with Redux/my-first-react-client/src/Layout.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ErrorBoundary from "./ErrorBoundary";
+import {App, AppHeader, Title, AppLogo} from './styles';
+import logo from './logo.svg';
+
+export default function Layout({ title, renderContent }) {
+	return(
+
+        <App>
+            <AppHeader>
+                <AppLogo src={logo} alt="logo" />
+                <Title>{title}</Title>
+            </AppHeader>
+            {renderContent()}
+        </App>
+    );
+}
+
+export function withLayout(title) {
+	return Component => props => (
+        <Layout 
+            title={title}
+            renderContent={() => (
+                <ErrorBoundary>
+                    <Component {...props}/>
+                </ErrorBoundary>
+            )}
+        />
+    );
+
+}
