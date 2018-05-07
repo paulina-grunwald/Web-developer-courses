393dd9be768b4c5f3dbbc68a65895839a3b785d7
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-05-07 19:00:58 +0200

    Update file

--- React/Learn React with Redux/my-first-react-client/src/Home.js
+++ React/Learn React with Redux/my-first-react-client/src/Home.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import Layout from './Layout'
-import { AppIntro} from './styles';
+import { withLayout } from './Layout';
+import { AppIntro } from './styles';
 
-export default function Home() {
-	return(
-      <Layout
-        title="Welcome to React"
-        renderContent={() => (
-          <React.Fragment>
-            <AppIntro>
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </AppIntro>
-            <h2>Hot reloading is amazing!</h2>
-          </React.Fragment>
-        )} 
-      />
+export function Home() {
+  // test error 
+ //throw new Error();
+	return (
+    <React.Fragment>
+      <AppIntro>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </AppIntro>
+      <h2>Hot reloading is amazing!</h2>
+    </React.Fragment>
     );
 }
+
+export default withLayout("Welcome to React")(Home);
