27e207da3df7219183a7f46d7d42a867d73d95de
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-05-21 07:25:02 +0200

    add javascript exercise file

---
+++ Javascript/Various Exercises/aboutme.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var myName = "Paulina";
+console.log(myName);
+
+ var myAge = 30, myFavouriteFruit = "Mango", myFavouriteAnimal = "dog";
+console.log("${myName} is ${myAge} years old. That means I am ${myAge * 12} months old. I like ${myFavouriteFruit} and ${myFavouriteAnimal}");
