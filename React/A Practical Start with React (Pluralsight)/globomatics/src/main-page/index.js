dc5149c8afde7eb34a6ca1b6c2aad83e431389c0
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-05-24 15:44:06 +0200

    update index.js

--- React/A Practical Start with React (Pluralsight)/globomatics/src/main-page/index.js
+++ React/A Practical Start with React (Pluralsight)/globomatics/src/main-page/index.js
@@ -4,11 +4,19 @@ import './main-page.css';
 import Header from './header';
 
 class App extends Component {
+  fetchHouses = () => {
+    fetch('/houses.json')
+    .then(rsp => rsp.json())
+    .then(allHouses => {
+      this.allHouses = allHouses;
+    })
+  }
+
   render() {
     return (
-     <div className="container">
-      <Header />
-    </div>
+      <div className="container-fluid">
+        <Header subtitle="Providing houses world wide"/>
+      </div>
     );
   }
 }
