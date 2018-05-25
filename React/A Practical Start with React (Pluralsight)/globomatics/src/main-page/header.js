c74dadef647c8c7a2206f28d80314e6cec765925
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-05-25 09:30:36 +0200

    update header.js

--- React/A Practical Start with React (Pluralsight)/globomatics/src/main-page/header.js
+++ React/A Practical Start with React (Pluralsight)/globomatics/src/main-page/header.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './GloboLogo.png';
 
-const Header = () => (
-    <header className="row">
-        <div className="col-md-5">
+const Header = (props) => (
+    <header className="row display-inline">
+        <div className="col-md-5 col-lg-5">
             <img src={logo} className="logo" alt="logo" />
         </div>
-        <div className="col-md-7 mt-5 subtitle">
-            Providing house world wide
+        <div className="col-md-7 col-lg-7 subtitle">
+            {props.subtitle}
         </div>
     </header>
 );
